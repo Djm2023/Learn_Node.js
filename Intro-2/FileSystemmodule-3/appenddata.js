1b4367b91b9eb16a468a26654cfc6215a41aef57
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2023-09-09 20:42:10 +0530

    Learn to DELETE a file from File Module

--- Intro-2/FileSystemmodule-3/appenddata.js
+++ Intro-2/FileSystemmodule-3/appenddata.js
@@ -3,3 +3,7 @@ const fs = require('fs');
 
 /*Append Data to existing file or if file doesnot exists then it will create a file */
 fs.appendFileSync('employee1.txt' ,"Name:Devjyoti mukherjee , Learn To append data to a text file");
+
+/** Delete a file */
+
+fs.unlink("employee1.txt");
