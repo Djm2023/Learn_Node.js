8154daa6692bec4b107d8db21fdea295edf4b309
Devjyoti Mukherjee <devjyotidgr2023@gmail.com>
2023-09-09 20:30:07 +0530

    Learn to Write a File in a blocking Mode of Code

---
+++ Intro-2/FileSystemmodule-2/fileOperation2.js
@@ -0,0 +1,6 @@
+/*Import Filesystem Module */
+const fs = require('fs');
+
+/*Creating and Writing a file  */
+
+fs.writeFileSync('employee.txt','Name:Devjyoti Mukherjee  , Dept : Manager , Position : MERN-stack Developer');
